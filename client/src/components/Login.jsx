bcd387580a28a859e9fe1daebf7d0538115cf6c9
Ankit Ydv <ankityadav11241@gmail.com>
2023-05-12 01:12:41 +0530

    refract

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <div>Login</div>
+    <div className="container">
+      <h2>Login</h2>
+      <div className="formContent">
+        <div className="inputBox">
+          <label>Username</label>
+          <input type="text" onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div className="inputBox">
+          <label>Password</label>
+          <input type="text" onChange={(e) => setPassword(e.target.value)} />
+        </div>
+          {console.log(username, password)}
+      </div>
+    </div>
   )
 }
 
