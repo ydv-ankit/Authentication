ac0a6abf5fa820497b279951afb5530efc25460d
Ankit Ydv <ankityadav11241@gmail.com>
2023-05-12 01:33:30 +0530

    Signup page

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Signup = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <div>Signup</div>
+    <div className="container">
+      <h2>Sign Up</h2>
+      <div className="formContent">
+        <div className="inputBox">
+          <label>Username</label>
+          <input type="text" onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div className="inputBox">
+          <label>Password</label>
+          <input type="text" onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button className='btn link' onClick={() => (
+          alert(username + " " + password)
+        )}>
+          Create User
+        </button>
+      </div>
+    </div>
   )
 }
 
