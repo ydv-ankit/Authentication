3f1de741bbec005c111c0638b0dd12abb037aa0d
Ankit Ydv <ankityadav11241@gmail.com>
2023-05-12 01:12:03 +0530

    homepage done

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import './Home.css'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
   return (
     <div className="container">
       <h1>Welcome !!</h1>
       <div className="btn">
-        <Button innerText="Sign Up" />
-        <Button innerText="Login" />
+        <Link to="/signup" className='link'>Sign Up</Link>
+        <Link to="/login" className='link'>Login</Link>
       </div>
     </div>
   )
-}
-
-const Button = ({ innerText }) => {
-  return (
-      <button>
-        {innerText}
-      </button>
-  );
 }
