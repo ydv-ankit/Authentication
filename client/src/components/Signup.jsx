832d5f866e51c8cd82e73692994158b70045d20c
Ankit Ydv <ankityadav11241@gmail.com>
2023-05-13 15:58:46 +0530

    user messages

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -3,18 +3,20 @@ import React, { useState } from 'react'
 const Signup = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [message, setMessage] = useState('');
 
   return (
     <div className="container">
       <h2>Sign Up</h2>
+      <h4>{message}</h4>
       <div className="formContent">
         <div className="inputBox">
           <label>Username</label>
-          <input type="text" name='username' onChange={(e) => setUsername(e.target.value)} placeholder='username'/>
+          <input type="text" name='username' onChange={(e) =>( setUsername(e.target.value, setMessage('')))} placeholder='username'/>
         </div>
         <div className="inputBox">
           <label>Password</label>
-          <input type="text" name='password' onChange={(e) => setPassword(e.target.value)} placeholder='password'/>
+          <input type="text" name='password' onChange={(e) =>( setPassword(e.target.value), setMessage(''))} placeholder='password'/>
         </div>
         <button
           type='submit'
@@ -29,9 +31,9 @@ const Signup = () => {
                 pass: password,
               }),
             });
-            console.log(response);
             const json = await response.json();
-            console.log(json);
+            console.log(json.msg)
+            setMessage(json.msg)
           }}>
           Sign Up
         </button>
